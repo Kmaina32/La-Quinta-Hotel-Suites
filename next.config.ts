710b2f75ffdafa544e9f5421098f10545023bfd6
kishan san <kishansan28@gmail.com>
2025-08-18 10:03:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cf.bstatic.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };

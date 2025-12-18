3ad51f355b8f7b7f37159f627a9887b8d02e4391
kairo kairo <GMaina424@gmail.com>
2025-12-18 11:05:26 +0000

    Refresh

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -5,25 +5,19 @@ import admin from 'firebase-admin';
 let db: admin.firestore.Firestore;
 let storage: admin.storage.Storage;
 
+
+const serviceAccount: admin.ServiceAccount = {
+  projectId: "la-quinta-reservations",
+  privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDEZXnhjlASZXaF\n0OiOyODg4zZbV+an4i1DUVGFmO+ybjPhzO5SN2n6amtn8WydIKmTmwizCbC63LJM\nzMyVdqlCKG0kOmPVfegMY732qBsEIOrqGe9cI0brkUsSzcD9d2iQpREmq2vk2PRu\nSY4x38IOpoJVI5zwzikKK2KEQdLPSYvGZRoJkmeLecoARyoZsyGfmsxwpD96+NcC\nE88rqtBhbsgQf4U0MqVYmsuwRqxpRb3b9EbVxitzQVjv5/OcZAGCs0iFsqbvG8fg\ni4x4mCs9q0ch/2kGVSfuOG7+cfqT0/aR2LX7SwJgMon3kDo8BVbyI6RvhtNJIPdB\n13JvOMJrAgMBAAECggEAKZGu+AJmyVYHfbBnoN081+zPswrKa+twGCkMn8HsrjAs\nNBVrtiQ7fsDqpfAQRLvGmNL+RuL8sLiI1r+cZ+BbWAD1hO8E9Ym2RXURHrWbLkcH\nYfCGXNKsrqysnrjFKgENtVvy4uPyrNgM+JECE2VgjjhJKtskW0XPovLm4caK58uy\nxquhAwb416cMkqGqc8fxV6IHngAG30QmzMldKvDPhNPRp4XHzgYF7IqekIYBwTQU\n8xvHZ/5EfUtHd7pFWWTVsKmd8X+OYMM/jMGoblJy2VZHTUBziS4UWUdHakT36dPD\nL4EEUqnMk/qMonBULZSUUZApvhQqqayw4XExjeAOmQKBgQDsonaqTg94s25oChZ9\nfG7CcJzVNfK+LI26U93mnBjHvpCfmz3PZVxKMXhNN+yL2RcBHf4b1WTsfZAHP8NB\njfXWWwu6j4ZhHPgPL2DbSJlfzyqLcTDWDOgpD2YtBz9jIi4Oj8419qfujke0Gt1Q\nwa1Hkoh6rqY6JNUsBSe9nGN1qQKBgQDUeAO9UJnwjtXGkPy2XmuPF3R2G4nbTnRy\n9++KOyz3EHwiUxtRSV+IqXbCEYvsVNV+qi7HW3r0R68I+Y2oU9Yrt8PHmEOe6DDp\nPPXaCclFjZQzY9EY33ducYAJX0SjJfOCf/5N79X7O4+sn9gF6OBHb/w/8PReT1FP\nmpWl1jVb8wKBgQCV4VlUCVlXVB8sGqegF753vag4i5ESz1l9mT1Fob04VwV3cna1\n9Dd+btMJ5dAXzAr8FCktK/5epDEjxklALlB10vkc02eD/ztHMvUgL12mB6uk4q7S\nBR7PUweeoaaOfcm6Q2+TUoWPXeMguptPWT0NxkxXmGOFFGtJFILVYEbG8QKBgGqQ\nPx/mmy8CFMpg0b8OIFhGZU0PhtcNxG4dWHE2ONk51WjJ0fu1F0tN45h2gH1qFyJO\nbOPkSAjZIzsXHyt70QFgS7uB7Ph4cH+q6YwQOHjAB8K2n5sgCaDFIHiS5bQGRtn/\nJCcm0WYOe4MTMJ/WKxbpXdUcHxRmJ9wLJl9kzqJtAoGBAKBUVu74DojACtOutFd4\nWMug/9ekYHfxs/HJ2A3Ek3Z3hTpYfeGrqfoWqz61yvnNAUgeWKxztLdYh6DzTjXB\nGakkCI9mjGJ4mbA8+nz/1MqFdTa3TcdfKwa6jG+Q0tNCZZGTQDychutkDcDCGUCE\ngIHlmZbfOV6C1fplJ6kb/mcC\n-----END PRIVATE KEY-----\n",
+  clientEmail: "firebase-adminsdk-fbsvc@la-quinta-reservations.iam.gserviceaccount.com"
+};
+
+let auth: admin.auth.Auth;
+
 function initializeAdmin() {
   // Check if the app is already initialized
   if (!admin.apps.length) {
     try {
-      const encoded = process.env.FIREBASE_SERVICE_ACCOUNT_JSON_BASE64;
-
-      if (!encoded) {
-        throw new Error(
-          'FIREBASE_SERVICE_ACCOUNT_JSON_BASE64 environment variable not found.'
-        );
-      }
-
-      // Decode Base64 to a string
-      const decoded = Buffer.from(encoded, 'base64').toString('utf-8');
-      
-      // IMPORTANT: Parse the decoded string into a JSON object first.
-      const serviceAccount = JSON.parse(decoded);
-
-      // Now, initialize the app, but correct the private key format within the cert object.
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
         storageBucket: 'la-quinta-reservations.appspot.com',
@@ -60,4 +54,15 @@ export function getStorage(): admin.storage.Storage {
   storage = admin.storage();
   return storage;
 }
-export default initializeAdmin;
+
+/**
+ * Returns an initialized Auth instance.
+ */
+export function getAuthAdmin(): admin.auth.Auth {
+  if (auth) {
+    return auth;
+  }
+  initializeAdmin();
+  auth = admin.auth();
+  return auth;
+}
